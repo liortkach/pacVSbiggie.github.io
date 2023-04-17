0257959ca37fd35e8407ad35b80e91d60603e94a
Lior Tkach <liortkach23@gmail.com>
2023-04-17 11:36:43 +0300

    lior commit 2

--- chicken.js
+++ chicken.js
@@ -7,6 +7,10 @@ var modalDialog;
 var users;
 var registerForm;
 var loginForm;
+var currentScreen;
+var currentMusic;
+var isMusicPlaying;
+var isLoggedIn;
 
 function setupGame(){
     users = {
@@ -20,15 +24,24 @@ function setupGame(){
     configureForm.addEventListener("submit", checkPreferences);
 
     homeScreen = document.getElementById("home");
-    homeScreen.style.display = "block";
     loginScreen = document.getElementById("login");
     configureScreen =  document.getElementById("configuration");
     registerScreen = document.getElementById("register");
     gameScreen = document.getElementById("game");
     modalDialog = document.getElementById('about');
+
+    isMusicPlaying = true;
+    isLoggedIn = false;
+    currentMusic = document.getElementById("bgmusic-home");
+    document.getElementById("bgmusic-game").pause();
+    currentScreen = homeScreen;
+    switchScreen("home");
+
     document.getElementById('close-modal').addEventListener('click', () => {
         modalDialog.close();
       });
+
+    document.getElementById("mute-btn").addEventListener("click", handleMuteMusic);
     
     document.addEventListener("click", function(event){
         if (event.target == modalDialog &&modalDialog.open){
@@ -44,7 +57,7 @@ function setupGame(){
 // update the configuration time range
 function updateTimeValue(value) {
     document.getElementById("rangeValue").innerHTML = value;
-  }
+}
 
 // validation of different input fields
 function validateDetails(inputValue, type="password") {
@@ -65,8 +78,31 @@ function validateDetails(inputValue, type="password") {
     return regex.test(inputValue);
   }
 
+  // mute music button
+  function handleMuteMusic(){
+
+    if (isMusicPlaying) {
+        // Mute the music
+        currentMusic.muted = true;
+        isMusicPlaying = false;
+      }
+       else {
+        currentMusic.muted = false;
+        isMusicPlaying = true;
+      }
+      
+      // Update the button img
+      var btn = document.getElementById("mute-btn");
+      if (isMusicPlaying) {
+        btn.style.backgroundImage = "url('images/speaker-filled-audio-tool.png')";
+    } else {
+        btn.style.backgroundImage = "url('images/volume-off-indicator.png')";
+      }
+ }
+  
   // check configuration setting
 function checkPreferences(event){
+    event.preventDefault();
     let shootKey = document.getElementById("shootKey").value;
     if(!validateDetails(shootKey,"keyboard")){
         alert("You can only choose one letter or the space key");
@@ -75,10 +111,8 @@ function checkPreferences(event){
     let gameTime = document.getElementById("timeGame").value;
 
     //Next define game configuration..
-    loginForm.reset();
-    configureScreen.style.display = "none";
-    gameScreen.style.display = "block";
-    
+    configureForm.reset();
+    switchScreen("game");
 }
 
 function registerUser(event) {
@@ -115,18 +149,16 @@ function registerUser(event) {
         users[username] = password;
         alert("Registration completed successfuly!");
         registerForm.reset();
-        registerScreen.style.display = "none";
-        loginScreen.style.display = "block";
-
+        switchScreen("login");
     }
+}
 
-  }
-
-  function loginUser(event){
+function loginUser(event){
+    event.preventDefault();
     let username = document.getElementById("loginUsername").value;
     let password = document.getElementById("loginPassword").value;
 
-    // check if password valid
+// check if password valid
     if(!(username  in users)){
         alert("Username don't exist.");
     }
@@ -134,38 +166,99 @@ function registerUser(event) {
         alert("Password is not correct.");
     }
     else{
-        // Need to figure out why screen wont change here 
         alert("you logged in successfuly!");
         loginForm.reset();
-        loginScreen.style.display = "none";
-        configureScreen.style.display = "block";
+        updateUserStatus();
+        switchScreen("config");
     }
+}
+// update button of logout to login and vice versa
+function updateUserStatus(){
+    let buttons = document.querySelectorAll('.login-btn');
+    if(!isLoggedIn){
+        isLoggedIn = true;
+        buttons.forEach( button => {
+            button.textContent = 'Log-out';
+        })
+    }
+    else {
+        isLoggedIn = false;
+        buttons.forEach( button => {
+            button.textContent = 'Log-in';
+        })
+        switchScreen("home");
+    }
+}
 
-  }
+function switchMusic(name){
+// only two kinds of audio for now   
+    currentMusic.pause();
+    currentMusic = document.getElementById("bgmusic-" + name);
+    console.log(currentMusic.id);
+    currentMusic.currentTime = 0;
+    currentMusic.play();
+}
+
 // Change screen by the menu buttons
-function switchScreen(event){
-    let className = event.className;
+function switchScreen(className){
+
+    // close all screens
     let screens = [homeScreen,loginScreen,registerScreen,gameScreen,configureScreen];
-    if (className != "aboutButton"){
+    if (className != "about-btn"){
         screens.forEach( function(screen){
             screen.style.display = "none";
         })
     }
-
-    if( className == "homeButton"){
-        homeScreen.style.display = "block";
-    }
-    else if( className == "loginButton"){
-        loginScreen.style.display = "block";
-    }
-    else if( className == "registerButton"){
-        registerScreen.style.display = "block";
-
-    }
-    else if(className == "aboutButton"){
-        modalDialog.showModal();
-    }
     
+    if(currentScreen.id == "game" && !className.includes("game")){
+        console.log(currentScreen.id);
+        switchMusic("home");
+    }
+    //show only the chosen one
+    switch(true) {
+        case className.includes("home"):
+            homeScreen.style.display = "block";
+            currentScreen = homeScreen;
+            break;
+
+        case className.includes("login"):
+            if(!isLoggedIn){
+                loginScreen.style.display = "block";
+                currentScreen = loginScreen;
+            }else {
+                updateUserStatus();
+            }
+            break;
+
+        case className.includes("register"):
+            registerScreen.style.display = "block";
+            currentScreen = registerScreen;
+            break;
+
+        case className.includes("config"):
+            configureScreen.style.display = "block";
+            currentScreen = configureScreen;            
+            break;
+
+        case className.includes("game"):
+            if(isLoggedIn){
+                gameScreen.style.display = "block";
+                if(currentScreen.id != "game"){
+                    currentScreen = gameScreen; 
+                    switchMusic("game");
+                }
+             }
+            else{
+                alert("You must login first.");
+                currentScreen.style.display = "block";
+            }
+            break;
+
+        default:
+            modalDialog.showModal();
+            break;
+    }
+
 }
 
 
--- gameBasicsJS.js
+++ gameBasicsJS.js
@@ -1,5 +1,7 @@
 var canvas
 var ctx
+var canvasWidth
+var canvasHeight
 
 var spaceship
 var spaceshipImage
@@ -13,53 +15,69 @@ var bullet
 var bulletImage
 var chickenVelocity
 var initialChickenVelocity
+var widthMoveSize
+var heightMoveSize
+var padding
+var startDrawChickensIndexX
+var startDrawChickensIndexY
 
+var TIME_INTERVAL = 25; // screen refresh interval in milliseconds
 var gamePoints
-var curTime
+var now
 var intervalTimer
+var delta
+var then
+var keysDown
+var keyUp
+
+
+window.addEventListener("load", setupGamePlay, false);
 
 
 function setupGamePlay() {
     // Get the canvas
-    canvas = document.getElementById("theCanvas");
+    canvas = document.getElementById("gameCanvas");
     ctx = canvas.getContext("2d");
 
+    canvasWidth = canvas.width
+    canvasHeight = canvas.height
 
     // start a new game
-    document.getElementById("submitButton").addEventListener("click", newGame, false)
+    document.getElementById("startButton").addEventListener("click", newGame, false)
 
-
-    // TODO: Set bg image
 
     bgImage = new Image()
+    bgImage.src = "images/world.jpg"
 
-
-    // TODO: Set spaceship Image
 
     spaceshipImage = new Image()
+    spaceshipImage.src = "images/spaceship.jpg"
 
-
-    // TODO: Set chicken image
 
     chickenImage = new Image()
+    chickenImage.src = "images/logo.png"
 
 
     // TODO: Set bullet image
 
     bulletImage = new Image()
-
+    bulletImage.src = "images/fireshot.jpg"
 
     // Game Objects
-    spaceship = { speed: 64 }
-    chickens2DArray = new Array(4)
+    spaceship = { speed: 256 }
+    chickens2DArray = new Array(5)
+    bullet = { speed: 256 }
+
+    keysDown = {}
 
     for (let i = 0; i < chickens2DArray.length; i++) {
-        chickens2DArray[i] = new Array(5)
+        chickens2DArray[i] = new Array(4)
     }
 
-    for (let i = 0; i < chickens2DArray.length; i++) {
-        for (let j = 0; j < chickens2DArray[i].length; j++) {
-            chickens2DArray[i][j] = new Object() // Objects for chickens
+    for (let col = 0; col < chickens2DArray.length; col++) {
+        for (let row = 0; row < chickens2DArray[col].length; row++) {
+            chickens2DArray[col][row] = new Object() // Objects for chickens
+            chickens2DArray[col][row].bgImage = new Image()
         }
     }
 
@@ -68,17 +86,17 @@ function setupGamePlay() {
 
 
     // Check for keys pressed where key represents the keycode captured
-    addEventListener("keydown", function (e) { keysDown[e.keyCode] = true; }, false);
+    document.addEventListener("keydown", function (e) { keysDown[e.keyCode] = true; }, false);
 
-    addEventListener("keyup", function (e) { delete keysDown[e.keyCode]; }, false);
+    document.addEventListener("keyup", function (e) { delete keysDown[e.keyCode]; }, false);
+
 }
 
 
 // set up interval timer to update game
-function startTimer()
-{
-   canvas.addEventListener( "click", fireCannonball, false );
-   intervalTimer = window.setInterval( updatePositions, TIME_INTERVAL );
+function startTimer() {
+    canvas.addEventListener("click", fireCannonball, false);
+    intervalTimer = window.setInterval(updatePositions, TIME_INTERVAL);
 } // end function startTimer
 
 
@@ -90,118 +108,228 @@ function degreesToRadians(degrees) {
 // Reset the spaceship and chickens positions
 function reset() {
     // Reset spaceship position
-    spaceship.x = canvas.width / 2
-    spaceship.y = canvas.height * 0.99
+    spaceship.x = canvasWidth / 2.2
+    spaceship.y = canvasHeight * 0.9
+    spaceship.speed = 256
+
+    defualtChickenCoordinateX = (canvasWidth / 11) * 4
+    defualtChickenCoordinateY = (canvasHeight / 13)
+
+    padding = canvasWidth * 0.01
+
+    startDrawChickensIndexX = defualtChickenCoordinateX
+    startDrawChickensIndexY = defualtChickenCoordinateY
+
+    initialChickenVelocity = canvasWidth / 2
+
+    setDefaultBullet()
+    setDefaultChickens()
+}
+
+
+function setDefaultChickens() {
+    for (let col = 0; col < chickens2DArray.length; col++) {
+        for (let row = 0; row < chickens2DArray[col].length; row++) {
+
+            // Draw Chickens in defualt 5X4
+
+            chickens2DArray[col][row].x = defualtChickenCoordinateX + col * (chickenImage.width + padding)
+            chickens2DArray[col][row].y = defualtChickenCoordinateY + row * (chickenImage.height + padding)
+            chickens2DArray[col][row].visiable = true
+        }
+    }
+}
+
+
+function setDefaultBullet() {
+    bullet.x = spaceship.x + spaceshipImage.width / 2 - bulletImage.width / 2
+    bullet.y = spaceship.y - 30
 }
 
 
 // Blank the chicken who got shot
-function hideChicken(i, j) {
-    chickens2DArray[i][j].bgImage.src = ''
+function hideChicken(chicken) {
+    chicken.visiable = false
 }
 
 // Update game objects - change player position based on key pressed
 
 function updatePositions(modifier) {
-    if ((38 in keysDown)) { // Player holding up
+
+    if ((38 in keysDown && spaceship.y > canvasHeight * 0.6)) { // Player holding up
         spaceship.y -= spaceship.speed * modifier;
     }
-    if ((40 in keysDown)) { // Player holding down
+    if ((40 in keysDown && spaceship.y < canvasHeight * 0.9)) { // Player holding down
         spaceship.y += spaceship.speed * modifier;
     }
-    if (37 in keysDown) { // Player holding left
+    if (37 in keysDown && spaceship.x > padding) { // Player holding left
         spaceship.x -= spaceship.speed * modifier;
     }
-    if (39 in keysDown) { // Player holding right
+    if (39 in keysDown && spaceship.x + 25 < canvasWidth - padding) { // Player holding right
         spaceship.x += spaceship.speed * modifier;
     }
-
-
-    // Check if bullet and chicken collider
-    if (
-        bullet.x <= (monster.x + 32)
-        && monster.x <= (bullet.x + 32)
-        && bullet.y <= (monster.y + 32)
-        && monster.y <= (bullet.y + 32)
-    ) {
-        ++gamePoints;
+    if (68 in keysDown) { // Player holding d for shoot
+        bullet.visiable = true
     }
+
+    var chickenUpdate = TIME_INTERVAL / 15000.0 * chickenVelocity;
+    startDrawChickensIndexX += chickenUpdate;
+
+    if (bullet.visiable) {
+        bullet.y -= bullet.speed * modifier
+    }
+
+
+    if (bullet.y < bulletImage.height) {
+        bullet.visiable = false
+        setDefaultBullet()
+    }
+    
+
+    if (bullet.visiable == false)
+    {
+        setDefaultBullet()
+    }
+
+    
+    collisionDetection()
+}
+// Check if bullet and chicken collider
+function collisionDetection() {
+    chickens2DArray.forEach(chickenArr => {
+        chickenArr.forEach(chicken => {
+            if (
+                bullet.visiable == true
+                && chicken.visiable == true
+                && bullet.x <= (chicken.x + 32)
+                && chicken.x <= (bullet.x + 32)
+                && bullet.y <= (chicken.y + 32)
+                && chicken.y <= (bullet.y + 32)
+            ) {
+                ++gamePoints;
+                bullet.visiable = false
+                setDefaultBullet()
+                hideChicken(chicken)
+                setTimeout(100)
+                return
+            }
+        })
+    });
 }
 
 
 function stopTimer() {
-    canvas.removeEventListener( "click", spaceshipShoot, false );
-    window.clearInterval( intervalTimer );
+    window.clearInterval(intervalTimer);
 }
 
 
 function newGame() {
-    reset()
-    s
+    reset();
+
+    chickenImage.width = 20
+    chickenImage.height = 20
+
+    spaceshipImage.width = 30
+    spaceshipImage.height = 30
+
+    bulletImage.width = 10
+    bulletImage.height = 10
+    bullet.visiable = false
+
+    chickenVelocity = initialChickenVelocity
+    startDrawChickensIndexX = defualtChickenCoordinateX
+    startDrawChickensIndexY = defualtChickenCoordinateY
+
+    then = Date.now();
+    intervalTimer = setInterval(main, 1);
+
+    for (let col = 0; col < chickens2DArray.length; col++) {
+        for (let row = 0; row < chickens2DArray[col].length; row++) {
+            chickens2DArray[col][row].bgImage = chickenImage
+        }
+    }
 }
-
-// TODO: To add the coordinate Y so the shoot will be from the top
-function spaceshipShoot() {
-
-    ctx.drawImage(bulletImage, spaceships.x, spaceship.y)
-    bullet.x = spaceship.x
-    bullet.y = spaceship.y
-}
-
 
 function main() {
 
-    curTime = Date.now()
+    now = Date.now()
+    delta = now - then
 
+    updatePositions(delta / 1000)
+    draw()
+
+    then = now
+}
+
+
+function draw() {
+    clear()
+    ctx.drawImage(bgImage, 0, 0)
+    ctx.drawImage(spaceshipImage, spaceship.x, spaceship.y, spaceshipImage.width, spaceshipImage.height)
+    drawChickens()
+
+    if (bullet.visiable) {
+        drawBullet()
+    }
 }
 
 
 function drawChickens() {
 
     // if the blocker hit the top or bottom, reverse direction
-    if (blocker.start.x < 0 || blocker.end.x > canvasHeight)
+    if (startDrawChickensIndexX - padding < 0 || startDrawChickensIndexX + 5 * (chickenImage.width + padding) > canvasWidth)
         chickenVelocity *= -1;
 
-    // if the target hit the top or bottom, reverse direction
-    if (target.start.y < 0 || target.end.y > canvasHeight)
-        chickenVelocity *= -1;
-}
+    for (let col = 0; col < chickens2DArray.length; col++) {
+        for (let row = 0; row < chickens2DArray[col].length; row++) {
 
-
-function drawChickensDefault() {
-
-    for (let i = 0; i < chickens2DArray.length; i++) {
-        for (let j = 0; j < chickens2DArray[i].length; j++) {
             // Draw Chickens in defualt 5X4
-            ctx.drawImage(chickenImage, defualtChickenCoordinateX * i, defualtChickenCoordinateY * j)
+
+            chickens2DArray[col][row].x = startDrawChickensIndexX + col * (chickenImage.width + padding)
+            chickens2DArray[col][row].y = startDrawChickensIndexY + row * (chickenImage.height + padding)
+            
+            if (chickens2DArray[col][row].visiable == true){
+                ctx.drawImage(chickens2DArray[col][row].bgImage, chickens2DArray[col][row].x,
+                    chickens2DArray[col][row].y, chickenImage.width, chickenImage.height)
+            }
         }
     }
 }
 
-function drawSmile() {
 
-    canvas = document.getElementById("canvas");
-    ctx = canvas.getContext("2d");
+function drawBullet() {
 
-    ctx.clearRect(147, 220, 170, 115);
-
-    ctx.beginPath();
-    ctx.arc(225, 225 + 150 / 6, 150 / 2, 0, degreesToRadians(180), false)
-    ctx.stroke()
+    ctx.drawImage(bulletImage, bullet.x, bullet.y, bulletImage.width, bulletImage.height)
 }
 
-function drawSad() {
+// function drawChickensDefault() {
 
-    var canvas = document.getElementById("canvas");
-    var ctx = canvas.getContext("2d");
+//     chickenImage.width = 20
+//     chickenImage.height = 20
 
-    ctx.clearRect(147, 220, 170, 115);
-
-    ctx.beginPath();
-    ctx.arc(225, 225 + 150 / 2, 150 / 2, 0, degreesToRadians(180), true)
-    ctx.stroke()
-}
-
-// function smile() {
-//     document.addEventListener("click", drawSmile, false)
+//     for (let col = 0; col < chickens2DArray.length; col++) {
+//         for (let row = 0; row < chickens2DArray[col].length; row++) {
+//             // Draw Chickens in defualt 5X4
+//             ctx.drawImage(chickenImage, defualtChickenCoordinateX + col * (chickenImage.width + padding),
+//                 defualtChickenCoordinateY + row * (chickenImage.height + padding), chickenImage.width, chickenImage.height)
+//         }
+//     }
 // }
+
+function clear() {
+    ctx.fillStyle = '#d0e7f9';
+    //set active color to #d0e... (nice blue)  
+    //UPDATE - as 'Ped7g' noticed - using clearRect() in here is useless, we cover whole surface of the canvas with //blue rectangle two lines below. I just forget to remove that line  
+    //ctx.clearRect(0, 0, canvas.width, canvas.height);  
+    //clear whole surface  
+    ctx.beginPath();
+    //start drawing  
+    ctx.rect(0, 0, canvas.width, canvas.height);
+    //draw rectangle from point (0, 0) to  
+    //(width, height) covering whole canvas  
+    ctx.closePath();
+    //end drawing  
+    ctx.fill();
+    //fill rectangle with active  
+    //color selected before  
+};
